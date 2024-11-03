9abeeddf9cc80c3f1f0b88e8cc08173a84a14159
kshitij-golokait <kshitij@golokait.com>
2024-11-03 19:13:21 +0530

    Converted client folder from submodule to regular folder

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+  {
+    name: "BusinessInIndia",
+    shares: 97,
+  },
+  {
+    name: "TataSteel",
+    shares: 80.5,
+  },
+  {
+    name: "RelianceStores",
+    shares: 75.5,
+  },
+  {
+    name: "MukeshAmbani",
+    shares: 72,
+  },
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "StartupLife",
+    shares: 20,
+  },
+];
